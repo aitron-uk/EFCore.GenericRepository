ac873c8ad44fe715a8275cbae749f26b28759a57
Waqas Aziz <waqasazizuae@hotmail.com>
2025-05-31 20:29:51 +0100

    updates IEntity to support created by and modified by props

--- src/Aitron.EFCore.QueryRepository/Entities/IEntity.cs
+++ src/Aitron.EFCore.QueryRepository/Entities/IEntity.cs
@@ -2,11 +2,40 @@ using System;
 
 namespace Hazelnut.EFCore.GenericRepository.Entities;
 
+/// <summary>
+///   <br />
+/// </summary>
 public interface IEntity
 {
+    /// <summary>Gets or sets the identifier.</summary>
+    /// <value>The identifier.</value>
     Guid Id { get; set; }
 
+    /// <summary>Gets or sets the date created.</summary>
+    /// <value>The date created.</value>
     DateTime DateCreated { get; set; }
 
-    DateTime DateModified { get; set; }
+    /// <summary>
+    /// Gets or sets the created by.
+    /// </summary>
+    /// <value>
+    /// The created by.
+    /// </value>
+    Guid CreatedBy { get; set; }
+
+    /// <summary>
+    /// Gets or sets the date modified.
+    /// </summary>
+    /// <value>
+    /// The date modified.
+    /// </value>
+    DateTime? DateModified { get; set; }
+
+    /// <summary>
+    /// Gets or sets the modified by.
+    /// </summary>
+    /// <value>
+    /// The modified by.
+    /// </value>
+    Guid? ModifiedBy { get; set; }
 }
